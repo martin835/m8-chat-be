7f56b089565a7670800f6f690c4ab205b86ddc90
martin835 <64438132+martin835@users.noreply.github.com>
2022-04-28 09:04:46 +0200

    example with chat windows routing

--- src/index.js
+++ src/index.js
@@ -3,48 +3,54 @@ import express from "express";
 import { Server } from "socket.io";
 import { createServer } from "http";
 
-let onlineUsers = []
+let onlineUsers = [];
 const app = express();
 
-app.use(express.json())
-app.use(cors())
+app.use(express.json());
+app.use(cors());
 
-app.get('/online-users', (req, res) => {
-    res.send(onlineUsers);
-})
+app.get("/online-users", (req, res) => {
+  res.send(onlineUsers);
+});
 
 const httpServer = createServer(app);
-const io = new Server(httpServer, { /* options */ });
-
+const io = new Server(httpServer, {
+  /* options */
+});
 
 io.on("connection", (socket) => {
-    console.log(socket.id)
-    console.log(socket.handshake.auth)
+  console.log("🔃 socketId on connection: ", socket.id);
+  console.log(socket.handshake.auth);
 
-    socket.emit("welcome")
+  socket.emit("welcome");
 
-    socket.on("setUsername", ({ username }) => {
-        console.log(username)
+  socket.on("setUsername", ({ username }) => {
+    console.log(username);
 
-        onlineUsers.push({ username, socketId: socket.id })
+    onlineUsers.push({ username, socketId: socket.id });
 
-        // How to emit an event to every other client socket
-        socket.broadcast.emit("userJoined")
+    // How to emit an event to every other client socket
+    socket.broadcast.emit("userJoined");
 
-        socket.emit("didLogin")
-    })
+    socket.emit("didLogin");
+  });
 
-    socket.on("outgoingMessage", ({ message }) => {
-        console.log(message)
+  socket.on("outgoingMessage", ({ message }) => {
+    console.log(message);
 
-        socket.broadcast.emit("incomingMessage", { message })
-    })
+    if (message.recipient) {
+      socket.to(message.recipient).emit("incomingMessage", { message });
+    } else {
+      socket.broadcast.emit("incomingMessage", { message });
+    }
+  });
 
-    socket.on("disconnect", () => {
-        onlineUsers = onlineUsers.filter(user => user.socketId !== socket.id)
-    })
+  socket.on("disconnect", () => {
+    console.log(`❌ Closed connection `); // -->> this is breaking the code, probably because the connection with that id doesn't exist at this point already with socketId: ${user.socketId}
+    onlineUsers = onlineUsers.filter((user) => user.socketId !== socket.id);
+  });
 });
 
 httpServer.listen(3030, () => {
-    console.log("listening on port 3030");
+  console.log("listening on port 3030");
 });
